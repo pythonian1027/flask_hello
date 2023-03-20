de4bb530ff0eb7f44992e35b67776dbb34768ee5
Ricardo Cortez <ricardocortez21@gmail.com>
2023-03-19 23:39:28 -0400

    removed webhook

--- api/webhook.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import time
-
-webhook_url = 'https://flask-hello-git-main-pythonian1027.vercel.app/webhook'
-
-data = {'name': 'rich'}
-
-r = requests.post(webhook_url, data=json.dumps(data), headers = {'Content-Type': 'application/json'})
