6bcd6308f04b85402cbbbff0f75123c0d448023e
Ricardo Cortez <ricardocortez21@gmail.com>
2023-03-20 01:09:01 -0400

    bringin index a level up and renaming to app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    payload = request.get_json()
+    print ('received')
+    # return 'Webhook received'
+    if request.method=='POST':
+        print (request.json)
+        return 'success', 200
+    else: 
+        abort(400)
+        
+@app.route('/')
+def home():
+    return 'Hello, World 4!'
+
+# @app.route('/about')
+# def about():
+#     return 'About'
